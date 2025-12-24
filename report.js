9ff5c614d99b22b648d90dd17901fc27a91f4690
52330918-cloud <52330918@students.liu.edu.lb>
2025-12-24 20:59:17 +0200

    api to let the student add a new report and send it to the administrator

--- report.js
+++ report.js
@@ -16,3 +16,44 @@ const q = "SELECT * FROM report";
     }
   });
 });
+//add new report
+app.post("/addreports", (req, res) => {
+  if (!req.body) {
+    return res.status(400).send("Request body is missing");
+  }
+
+  const { name, email, message } = req.body;
+
+  const errors = [];
+    if (!name) {
+    errors.push("Name is required");
+  }
+  if (!email) {
+    errors.push("Email is required");
+  }
+  if (!message) {
+    errors.push("Message is required");
+  }
+  
+  if (errors.length > 0) {
+    return res.status(400).json({ message: errors });
+  }
+  const q =
+    "INSERT INTO report (name, email, message) VALUES (?, ?, ?)";
+
+  db.query(q, [name, email, message], (err, data) => {
+    if (err) {
+      if (err.errno === 1062) {
+        return res.status(400).json({ message: err.sqlMessage });
+      }
+      return res.status(500).json({ message: "Database error", error: err });
+    } else {
+      return res.status(201).json({
+        message: "Student added successfully",
+        id: data.insertId,
+
+        
+      });
+    }
+  });
+});
