847a9334a1e1b7c23b480386e5429d3cdc33baf2
52330918-cloud <52330918@students.liu.edu.lb>
2025-12-24 20:42:48 +0200

    api to add a new course or a new student by the administrator

--- course.js
+++ course.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express.Router();
 app.get("/getcourses", (req, res) => {
   const q = "SELECT * FROM course";
-
+//get all courses
   db.query(q, (err, data) => {
     if (err) {
       return res.status(500).json({ message: "Database error", error: err });
@@ -15,6 +15,41 @@ app.get("/getcourses", (req, res) => {
     }
   });
 });
+//add new course
+app.post("/addstudents", (req, res) => {
+  if (!req.body) {
+    return res.status(400).send("Request body is missing");
+  }
 
+  const { name, description } = req.body;
+
+  const errors = [];
+    if (!name) {
+    errors.push("course name is required");
+  }
+  if (!description) {
+    errors.push("course description is required");
+  }
+  if (errors.length > 0) {
+    return res.status(400).json({ message: errors });
+  }
+  const q =
+    "INSERT INTO course (name, description) VALUES (?, ?)";
+
+  db.query(q, [name, description], (err, data) => {
+    if (err) {
+      if (err.errno === 1062) {
+        return res.status(400).json({ message: err.sqlMessage });
+      }
+      return res.status(500).json({ message: "Database error", error: err });
+    } else {
+      return res.status(201).json({
+        message: "course added successfully",
+         id: data.insertId,
+        
+      });
+    }
+  });
+});
 
 export default app;
--- student.js
+++ student.js
@@ -3,8 +3,9 @@ import db from "./db.js";
 import express from "express";
 const app = express.Router();
 app.get("/getstudents", (req, res) => {
-  const q = "SELECT * FROM student";
+const q = "SELECT * FROM student";
 
+//get all students
   db.query(q, (err, data) => {
     if (err) {
       return res.status(500).json({ message: "Database error", error: err });
@@ -13,6 +14,52 @@ app.get("/getstudents", (req, res) => {
         return res.status(204).send("No students found");
       }
       return res.status(200).json(data);
+    }
+  });
+});
+//add new student
+app.post("/addstudents", (req, res) => {
+  if (!req.body) {
+    return res.status(400).send("Request body is missing");
+  }
+
+  const { student_id, password, fname, lname, major } = req.body;
+
+  const errors = [];
+    if (!student_id) {
+    errors.push("Student ID is required");
+  }
+  if (!password) {
+    errors.push("Password is required");
+  }
+  if (!fname) {
+    errors.push("First name is required");
+  }
+  if (!lname) {
+    errors.push("Last name is required");
+  }
+  if (!major) {
+    errors.push("Major is required");
+  }
+  if (errors.length > 0) {
+    return res.status(400).json({ message: errors });
+  }
+  const q =
+    "INSERT INTO student (student_id, password, fname, lname, major) VALUES (?, ?, ?, ?, ?)";
+
+  db.query(q, [student_id, password, fname, lname, major], (err, data) => {
+    if (err) {
+      if (err.errno === 1062) {
+        return res.status(400).json({ message: err.sqlMessage });
+      }
+      return res.status(500).json({ message: "Database error", error: err });
+    } else {
+      return res.status(201).json({
+        message: "Student added successfully",
+        
+
+        
+      });
     }
   });
 });
