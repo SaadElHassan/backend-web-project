ead8178d6ac7d53c6e917a5b68acec4aa646a1b5
52330918-cloud <52330918@students.liu.edu.lb>
2025-12-25 19:13:15 +0200

    fixing some url and adding delete api so that the administrator can remove courses

--- course.js
+++ course.js
@@ -31,7 +31,7 @@ app.get("/getcoursesnames", (req, res) => {
   });
 });
 //add new course
-app.post("/addstudents", (req, res) => {
+app.post("/addcourse", (req, res) => {
   if (!req.body) {
     return res.status(400).send("Request body is missing");
   }
@@ -63,6 +63,27 @@ app.post("/addstudents", (req, res) => {
          id: data.insertId,
         
       });
+    }
+  });
+});
+//delete course
+app.delete("/deletecourse/:name", (req, res) => {
+  const { name } = req.params;
+
+  if (!name) {
+    return res.status(400).json({ message: "Course name is required" });
+  }
+
+  const q = "DELETE FROM course WHERE name = ?";
+
+  db.query(q, [name], (err, data) => {
+    if (err) {
+      return res.status(500).json({ message: "Database error", error: err });
+    } else {
+      if (data.affectedRows === 0) {
+        return res.status(404).json({ message: "Student not found" });
+      }
+      return res.status(200).json({ message: "Student deleted successfully" });
     }
   });
 });
