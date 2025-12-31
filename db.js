0d80c212005e2602bef6a41c3d8aefdcaa0fbbc2
52330918-cloud <52330918@students.liu.edu.lb>
2025-12-31 11:18:23 +0200

    Add SSL support for Railway MySQL connection

--- db.js
+++ db.js
@@ -5,6 +5,7 @@ const db = mysql.createPool({
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "web2_project",
   port: process.env.DB_PORT || 3306,
+  ssl: process.env.DB_HOST && process.env.DB_HOST !== "localhost" ? { rejectUnauthorized: false } : false,
 });
 db.getConnection((err) => {
   if (err) {
